0775d639dec72e442704419a46fd4d68782869c9
[ysjang] <[jangtang2@gmail.com]>
2024-05-13 18:45:41 +0900

    [20240513-1845] -인체용 면역진단 페이지 완료

---
+++ js/showProds.js
@@ -0,0 +1,220 @@
+const btn1 = document.getElementById('btn1')
+const btn2 = document.getElementById('btn2')
+const btn3 = document.getElementById('btn3')
+const btn4 = document.getElementById('btn4')
+const btn5 = document.getElementById('btn5')
+const btn6 = document.getElementById('btn6')
+const btn7 = document.getElementById('btn7')
+const btn8 = document.getElementById('btn8')
+const btn9 = document.getElementById('btn9')
+
+const items1 = document.getElementById('items1')
+const items2 = document.getElementById('items2')
+const items3 = document.getElementById('items3')
+const items4 = document.getElementById('items4')
+const items5 = document.getElementById('items5')
+const items6 = document.getElementById('items6')
+const items7 = document.getElementById('items7')
+const items8 = document.getElementById('items8')
+const items9 = document.getElementById('items9')
+
+function no1(){
+    btn1.classList.add("active");
+    btn2.classList.remove("active");
+    btn3.classList.remove("active");
+    btn4.classList.remove("active");
+    btn5.classList.remove("active");
+    btn6.classList.remove("active");
+    btn7.classList.remove("active");
+    btn8.classList.remove("active");
+    btn9.classList.remove("active");
+
+    items1.classList.add("active");
+    items2.classList.remove("active");
+    items3.classList.remove("active");
+    items4.classList.remove("active");
+    items5.classList.remove("active");
+    items6.classList.remove("active");
+    items7.classList.remove("active");
+    items8.classList.remove("active");
+    items9.classList.remove("active");
+
+}
+function no2(){
+    btn1.classList.remove("active");
+    btn2.classList.add("active");
+    btn3.classList.remove("active");
+    btn4.classList.remove("active");
+    btn5.classList.remove("active");
+    btn6.classList.remove("active");
+    btn7.classList.remove("active");
+    btn8.classList.remove("active");
+    btn9.classList.remove("active");
+
+    items1.classList.remove("active");
+    items2.classList.add("active");
+    items3.classList.remove("active");
+    items4.classList.remove("active");
+    items5.classList.remove("active");
+    items6.classList.remove("active");
+    items7.classList.remove("active");
+    items8.classList.remove("active");
+    items9.classList.remove("active");
+}
+function no3(){
+    btn1.classList.remove("active");
+    btn2.classList.remove("active");
+    btn3.classList.add("active");
+    btn4.classList.remove("active");
+    btn5.classList.remove("active");
+    btn6.classList.remove("active");
+    btn7.classList.remove("active");
+    btn8.classList.remove("active");
+    btn9.classList.remove("active");
+
+    items1.classList.remove("active");
+    items2.classList.remove("active");
+    items3.classList.add("active");
+    items4.classList.remove("active");
+    items5.classList.remove("active");
+    items6.classList.remove("active");
+    items7.classList.remove("active");
+    items8.classList.remove("active");
+    items9.classList.remove("active");
+}
+function no4(){
+    btn1.classList.remove("active");
+    btn2.classList.remove("active");
+    btn3.classList.remove("active");
+    btn4.classList.add("active");
+    btn5.classList.remove("active");
+    btn6.classList.remove("active");
+    btn7.classList.remove("active");
+    btn8.classList.remove("active");
+    btn9.classList.remove("active");
+
+    items1.classList.remove("active");
+    items2.classList.remove("active");
+    items3.classList.remove("active");
+    items4.classList.add("active");
+    items5.classList.remove("active");
+    items6.classList.remove("active");
+    items7.classList.remove("active");
+    items8.classList.remove("active");
+    items9.classList.remove("active");
+}
+function no5(){
+    btn1.classList.remove("active");
+    btn2.classList.remove("active");
+    btn3.classList.remove("active");
+    btn4.classList.remove("active");
+    btn5.classList.add("active");
+    btn6.classList.remove("active");
+    btn7.classList.remove("active");
+    btn8.classList.remove("active");
+    btn9.classList.remove("active");
+
+    items1.classList.remove("active");
+    items2.classList.remove("active");
+    items3.classList.remove("active");
+    items4.classList.remove("active");
+    items5.classList.add("active");
+    items6.classList.remove("active");
+    items7.classList.remove("active");
+    items8.classList.remove("active");
+    items9.classList.remove("active");
+}
+function no6(){
+    btn1.classList.remove("active");
+    btn2.classList.remove("active");
+    btn3.classList.remove("active");
+    btn4.classList.remove("active");
+    btn5.classList.remove("active");
+    btn6.classList.add("active");
+    btn7.classList.remove("active");
+    btn8.classList.remove("active");
+    btn9.classList.remove("active");
+
+    items1.classList.remove("active");
+    items2.classList.remove("active");
+    items3.classList.remove("active");
+    items4.classList.remove("active");
+    items5.classList.remove("active");
+    items6.classList.add("active");
+    items7.classList.remove("active");
+    items8.classList.remove("active");
+    items9.classList.remove("active");
+}
+function no7(){
+    btn1.classList.remove("active");
+    btn2.classList.remove("active");
+    btn3.classList.remove("active");
+    btn4.classList.remove("active");
+    btn5.classList.remove("active");
+    btn6.classList.remove("active");
+    btn7.classList.add("active");
+    btn8.classList.remove("active");
+    btn9.classList.remove("active");
+
+    items1.classList.remove("active");
+    items2.classList.remove("active");
+    items3.classList.remove("active");
+    items4.classList.remove("active");
+    items5.classList.remove("active");
+    items6.classList.remove("active");
+    items7.classList.add("active");
+    items8.classList.remove("active");
+    items9.classList.remove("active");
+}
+function no8(){
+    btn1.classList.remove("active");
+    btn2.classList.remove("active");
+    btn3.classList.remove("active");
+    btn4.classList.remove("active");
+    btn5.classList.remove("active");
+    btn6.classList.remove("active");
+    btn7.classList.remove("active");
+    btn8.classList.add("active");
+    btn9.classList.remove("active");
+
+    items1.classList.remove("active");
+    items2.classList.remove("active");
+    items3.classList.remove("active");
+    items4.classList.remove("active");
+    items5.classList.remove("active");
+    items6.classList.remove("active");
+    items7.classList.remove("active");
+    items8.classList.add("active");
+    items9.classList.remove("active");
+}
+function no9(){
+    btn1.classList.remove("active");
+    btn2.classList.remove("active");
+    btn3.classList.remove("active");
+    btn4.classList.remove("active");
+    btn5.classList.remove("active");
+    btn6.classList.remove("active");
+    btn7.classList.remove("active");
+    btn8.classList.remove("active");
+    btn9.classList.add("active");
+
+    items1.classList.remove("active");
+    items2.classList.remove("active");
+    items3.classList.remove("active");
+    items4.classList.remove("active");
+    items5.classList.remove("active");
+    items6.classList.remove("active");
+    items7.classList.remove("active");
+    items8.classList.remove("active");
+    items9.classList.add("active");
+}
+
+btn1.addEventListener('click', no1)
+btn2.addEventListener('click', no2)
+btn3.addEventListener('click', no3)
+btn4.addEventListener('click', no4)
+btn5.addEventListener('click', no5)
+btn6.addEventListener('click', no6)
+btn7.addEventListener('click', no7)
+btn8.addEventListener('click', no8)
+btn9.addEventListener('click', no9)
