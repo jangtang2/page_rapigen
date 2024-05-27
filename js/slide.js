7aa7d0219d2d9e936ef7c79e11932aa00d276b05
jangtang2 <jangtang2@gmail.com>
2024-05-28 02:54:17 +0900

    update

--- js/slide.js
+++ js/slide.js
@@ -2,7 +2,7 @@ var swiper = new Swiper(".swiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 2500,
+      delay: 5000,
       disableOnInteraction: false,
     },
     pagination: {
