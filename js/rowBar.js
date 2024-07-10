5963c0e36f4512f6a513354667537a3fef7af96c
[ysjang] <[jangtang2@gmail.com]>
2024-07-10 16:33:51 +0900

    채용전형 가로 선 업데이트

--- js/rowBar.js
+++ js/rowBar.js
@@ -6,11 +6,15 @@ document.addEventListener('DOMContentLoaded', function() {
                 if (entry.target.classList.contains('no1')) {
                     setTimeout(() => {
                         entry.target.classList.add('animate');
-                    }, 700);
+                    }, 600);
                 } else if (entry.target.classList.contains('no2')) {
                     setTimeout(() => {
                         entry.target.classList.add('animate');
-                    }, 1600);
+                    }, 1200);
+                }else if (entry.target.classList.contains('no3')) {
+                    setTimeout(() => {
+                        entry.target.classList.add('animate');
+                    }, 1800);
                 }
             }
             else{
@@ -22,7 +26,9 @@ document.addEventListener('DOMContentLoaded', function() {
     // no1과 no2 요소를 관찰
     const no1 = document.querySelector('.between > .no1');
     const no2 = document.querySelector('.between > .no2');
+    const no3 = document.querySelector('.between > .no3');
 
     if (no1) observer.observe(no1);
     if (no2) observer.observe(no2);
+    if (no3) observer.observe(no3);
 });
