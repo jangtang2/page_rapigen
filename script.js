8a00eabccb65efd3c4baf3efaca5e3b969ec3db5
[ysjang] <[jangtang2@gmail.com]>
2024-07-11 10:00:55 +0900

    자동 슬라이트 버튼 하이라이트

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let currentPhotoIndex = 0;
 const photos = document.querySelectorAll('.photo');
 const totalPhotos = photos.length;
+var buttons = document.querySelectorAll('.button');
 
 function showPhoto(index) {
     hideCurrentPhoto();
@@ -45,6 +46,10 @@ function showCurrentPhoto() {
         subText.style.opacity = 1;
         subText.style.transform = 'translateY(0)';
     }, 1000); // Additional delay for the sub_text
+
+    // 모든 버튼의 active 클래스를 제거하고 현재 버튼에 active 클래스 추가
+    buttons.forEach(button => button.classList.remove('active'));
+    buttons[currentPhotoIndex].classList.add('active');
 }
 
 let intervalId = setInterval(showNextPhoto, 3000);
@@ -54,5 +59,6 @@ function resetTimer() {
     intervalId = setInterval(showNextPhoto, 3000);
 }
 
-// Initialize the first photo
+// Initialize the first photo and active button
 showCurrentPhoto();
+buttons[currentPhotoIndex].classList.add('active');
